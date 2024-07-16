776f43598cc3e157718c60fd5b171d0d0b0da714
walnut-ro <136340414+walnut-ro@users.noreply.github.com>
2024-07-16 22:08:31 +0300

    first files

---
+++ minishell.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlevinsc <dlevinsc@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 21:31:53 by dlevinsc          #+#    #+#             */
+/*   Updated: 2024/07/16 21:34:13 by dlevinsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_shell	shell;
+
+	(void)argc;
+	(void)argv;
+	(void)envp;
+}
